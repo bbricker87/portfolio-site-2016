dec0c0aca9ae4e12dd68f604296cb680aef52ff8
Ben Bricker <bbricker09@gmail.com>
2016-06-29 16:05:07 -0400

    React Actually Up and Running Now

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,31 @@
 var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
-  context: __dirname +"/src",
+  context: path.join(__dirname, "app"),
   devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/client.js",
+  entry: "./js/client.jsx",
   module: {
     loaders: [
       {
-        test: /\.js?&/,
+        test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-class-properites', 'transform-decorators-legacy']
+          plugins: ['transform-class-properties']
         }
       }
     ]
   },
   output: {
-    path: __dirname + "/src/",
+    path: __dirname + "/app/",
     filename: "client.min.js"
   },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false}),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
   ],
 };

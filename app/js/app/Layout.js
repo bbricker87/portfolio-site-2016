f6590d63f7dd51d104a673eae1a58727b1ee3faf
Ben Bricker <bbricker09@gmail.com>
2016-07-20 15:30:33 -0400

    Transitions in place

--- app/js/app/Layout.js
+++ app/js/app/Layout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { RouteTransition } from "react-router-transition";
 
 import Header from "./header/header";
 import Footer from "./footer/footer";
@@ -7,7 +8,18 @@ export default function(props) {
   return(
     <div>
       <Header title={props.children.props.route.title}/>
-      <main>{props.children}</main>
+      <main>
+        <RouteTransition className="route__transition"
+          pathname={props.location.pathname}
+          atEnter={{ translateX: -100 }}
+          atLeave={{ translateX: 100 }}
+          atActive={{ translateX: 0 }}
+          mapStyles={styles => ({ transform: `translateX(${styles.translateX}%)` })}
+        >
+          {props.children}
+        </RouteTransition>
+          <div className="route__placeholder">{props.children}</div>
+      </main>
       <Footer />
     </div>
   );
--- app/js/app/header/title.js
+++ app/js/app/header/title.js
@@ -1,17 +1,22 @@
 import React from "react";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
 const Title = React.createClass({
 
   render: function() {
     let characters = this.props.title.split("");
     return(
-        <h1 className="header__title header__title--page" onClick={this.props.toggleMenu}>{characters.map(this.renderCharacters)}</h1>
+        <ReactCSSTransitionGroup component="h1" transitionName="title-change"
+        transitionEnterTimeout={2000} transitionLeaveTimeout={2000} transitionAppear={true} transitionAppearTimeout={1000}
+        className="header__title header__title--page" onClick={this.props.toggleMenu}>
+          <div className="title" key={this.props.title}>{characters.map(this.renderCharacters)}</div>
+        </ReactCSSTransitionGroup>
     );
   },
 
   renderCharacters: function(titleCharacter, index) {
     return (
-      <span key={index}>{titleCharacter}</span>
+      <span className={"title__character title__character--" + index} key={this.props.title + "-" + index}>{titleCharacter}</span>
     );
   }
 });
